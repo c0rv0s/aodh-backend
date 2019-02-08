470e509d47aa174908a811a6519d0d8c002ca70f
Digital Ocean <inathan.m@gmail.com>
2019-02-08 10:09:22 +0000

    more results

--- server.js
+++ server.js
@@ -220,7 +220,7 @@ app.post('/api/list_ten', function(req, res) {
       var data
         var i = 0
         var miss = 0   
-        while(i < 8) {
+        while(i < 50) {
           var ranNum = Math.floor(Math.random() * table_height)
 	  if (!cur_d.includes(ranNum)) {
             id_array.push(ranNum)
@@ -233,7 +233,7 @@ app.post('/api/list_ten', function(req, res) {
 	    break
 	  }
         }
-        db.query('SELECT * FROM discover WHERE discoverable = 1 AND id IN($1,$2,$3,$4,$5,$6,$7,$8)',id_array, function(err,table) {
+        db.query('SELECT * FROM discover WHERE discoverable = 1 AND id IN($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42,$43,$44,$45,$46,$47,$48,$49,$50)',id_array, function(err,table) {
           if (err) {
             return res.status(400).send(err)
           }
